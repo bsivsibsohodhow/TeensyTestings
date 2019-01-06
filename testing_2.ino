7bf08fa2fa786be6e61c8c37d1792af189d8c262
Teri Ke <32941345+friedforest@users.noreply.github.com>
2019-01-06 16:23:07 -0500

    add audio system code

--- testing_2.ino
+++ testing_2.ino
@@ -1,1 +1,13 @@
-//testing
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioSynthWaveform       waveform1;      //xy=275,270
+AudioOutputI2S           i2s1;           //xy=518,262
+AudioConnection          patchCord1(waveform1, 0, i2s1, 0);
+AudioConnection          patchCord2(waveform1, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=315,423
+// GUItool: end automatically generated code
