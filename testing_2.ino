c843a478a92a3bb0c319040e91b813a9eea087d9
Teri Ke <32941345+friedforest@users.noreply.github.com>
2019-01-06 16:28:07 -0500

    add setup & loop

--- testing_2.ino
+++ testing_2.ino
@@ -11,3 +11,20 @@ AudioConnection          patchCord1(waveform1, 0, i2s1, 0);
 AudioConnection          patchCord2(waveform1, 0, i2s1, 1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=315,423
 // GUItool: end automatically generated code
+
+void setup() {
+  Serial.begin(9600);
+  AudioMemory(20);
+  sgt15000_1.enable();
+  sgt15000_1.volume(0.32);
+  waveform1.begin(WAVWFORM_SAWTOOTH);
+  waveform1.frequency(50);
+  waveform1.pulseWidth(0.15);
+}
+
+void loop() {
+  for (int freq = 50; freq <=500; freq++) {
+    waveform1.frequency(freq);
+    delay(10);
+  }
+}
